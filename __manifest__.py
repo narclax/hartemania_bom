260c53c5509521cd4093a6e8963ff015bc5a5116
Jose Marty <jose.m.marty@gmail.com>
2024-03-06 08:58:45 -0400

    Init  Push

---
+++ __manifest__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'autor': 'Jose Marty',
+    'name': 'Reportes Lista de Precios Hartemania',
+    'category': 'Reports',
+    'summary': 'Lista de Precios para Hartemania.',
+    'description': """
+Lista de Precios personalizados para Hartemania.
+""",
+    'depends': ['mrp'],
+    'data': [
+        #'security/ir.model.access.csv',        
+        #'reports/invoice_report.xml',
+        #reports/quotation_report.xml',
+        #'reports/template_view.xml',
+        #'reports/view_report.xml',
+        #'views/account_move_views.xml',
+        #'views/sale_views.xml',
+    ],
+    'demo': [
+        #'data/website_demo.xml'
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'OEEL-1',
+    'assets': {
+        'web.assets_frontend': [
+            #'jm_henca_website/static/src/css/fonts/fonts.css',
+            #'jm_henca_website/static/src/css/custom.css',
+        ],
+    }
+}
